d67ab0c5192fcbd06e59fee1c6d5c1118fa6cde2
soban.khan <soban.khan@primathon.in>
2023-01-05 10:58:34 +0530

    initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d
@@ -0,0 +1,5 @@
+/home/soban/personal/practice/rust/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c: src/main.rs
+
+/home/soban/personal/practice/rust/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/soban/personal/practice/rust/hello_cargo/target/debug/hello_cargo: /home/soban/personal/practice/rust/hello_cargo/src/main.rs
